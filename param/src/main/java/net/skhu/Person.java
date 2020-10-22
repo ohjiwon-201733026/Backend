511fe03b6c95486c872c12efa5f859c5b06e65a6
Oh,Jiwon <ojw970725@gmail.com>
2020-10-22 14:56:39 +0900

    파라미터전달

---
+++ param/src/main/java/net/skhu/Person.java
@@ -0,0 +1,32 @@
+package net.skhu;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Person {
+	String name;
+	double weight;
+	Date birthday;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	public void setBirthday(Date birthday) {
+		this.birthday=birthday;
+	}
+
+}
