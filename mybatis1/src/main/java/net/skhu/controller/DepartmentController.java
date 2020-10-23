ca436f10ba05b86ce379a59b3d4f22e97572b9fa
Oh,Jiwon <ojw970725@gmail.com>
2020-10-23 18:38:17 +0900

    mybatis 시작

--- mybatis1/src/main/java/net/skhu/controller/DepartmentController.java
+++ mybatis1/src/main/java/net/skhu/controller/DepartmentController.java
@@ -10,55 +10,50 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import net.skhu.Department;
+import net.skhu.dto.Department;
 import net.skhu.mapper.DepartmentMapper;
 
 @Controller
 @RequestMapping("/department")
 public class DepartmentController {
-	
-	@Autowired DepartmentMapper departmentMapper;
-	
-	@RequestMapping("list")
-	public String list(Model model) {
-		List<Department> departments=departmentMapper.findAll();
-		model.addAttribute("department",departments);
-		return "department/list";
-		
-	}
-	
-	@GetMapping("create")
-	public String create(Model model) { // insert view
-		model.addAttribute("department",new Department());
-		return "department/edit";
-		
-	}
-	
-	@PostMapping("create")
-	public String create(Model model,Department department) { //insert 기능
-		departmentMapper.insert(department);
-		return "redirect:list";
-	}
-	
-	
-	@GetMapping("edit")
-	public String edit(Model model , @RequestParam("id") int id) { // update view
-		Department department=departmentMapper.findOne(id);
-		model.addAttribute("department",department);
-		return "department/edit";
-	}
-	
-	@PostMapping("edit")
-	public String edit(Model model,Department department) { //update 기능
-		departmentMapper.update(department);
-		return "redirect:list";
-	}
-	
-	@RequestMapping("delete")
-	public String delete(Model model,@RequestParam("id")int id) {
-		departmentMapper.delete(id);
-		return "department/list";
-	}
-	
 
+    @Autowired DepartmentMapper departmentMapper;
+
+    @RequestMapping("list")
+    public String list(Model model) {
+        List<Department> departments = departmentMapper.findAll();
+        model.addAttribute("departments", departments);
+        return "department/list";
+    }
+
+    @GetMapping("create")
+    public String create(Model model) {
+        model.addAttribute("department", new Department());
+        return "department/edit";
+    }
+
+    @PostMapping("create")
+    public String create(Model model, Department department) {
+        departmentMapper.insert(department);
+        return "redirect:list";
+    }
+
+    @GetMapping("edit")
+    public String edit(Model model, @RequestParam("id") int id) {
+        Department department = departmentMapper.findOne(id);
+        model.addAttribute("department", department);
+        return "department/edit";
+    }
+
+    @PostMapping("edit")
+    public String edit(Model model, Department department) {
+        departmentMapper.update(department);
+        return "redirect:list";
+    }
+
+    @RequestMapping("delete")
+    public String delete(Model model, @RequestParam("id") int id) {
+        departmentMapper.delete(id);
+        return "redirect:list";
+    }
 }
--- mybatis1/src/main/java/net/skhu/mapper/DepartmentMapper.java
+++ mybatis1/src/main/java/net/skhu/mapper/DepartmentMapper.java
@@ -4,27 +4,29 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
-import net.skhu.Department;
+import net.skhu.dto.Department;
 
+@Mapper
 public interface DepartmentMapper {
-	@Select("SELECT * FROM department")
-	List<Department> findAll();
 
-	@Select("SELECT * FROM department WHERE id = #{id}")
-	Department findOne(int id);
+    @Select("SELECT * FROM department")
+    List<Department> findAll();
 
-	@Insert("INSERT department (departmentName) VALUES (#{departmentName})")
-	@Options(useGeneratedKeys = true, keyProperty = "id")
-	void insert(Department department);
+    @Select("SELECT * FROM department WHERE id = #{id}")
+    Department findOne(int id);
 
-	@Update("UPDATE department SET departmentName = #{departmentName} WHERE id = #{id}")
-	void update(Department department);
+    @Insert("INSERT department (departmentName) VALUES (#{departmentName})")
+    @Options(useGeneratedKeys=true, keyProperty="id")
+    void insert(Department department);
+   
+    @Update("UPDATE department SET departmentName = #{departmentName} WHERE id = #{id}")
+    void update(Department department);
 
-	@Delete("DELETE FROM department WHERE id = #{id}")
-	void delete(int id);
-
+    @Delete("DELETE FROM department WHERE id = #{id}")
+    void delete(int id);
 }
