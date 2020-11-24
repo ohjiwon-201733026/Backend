0b35c488e6acfa66d2a06a100d083b3f2bb09d41
OH JIWON <ojw970725@gmail.com>
2020-11-24 19:14:17 +0900

    resultMap4

---
+++ resultMap4/src/main/java/net/skhu/ResultMap4Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultMap4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultMap4Application.class, args);
+	}
+
+}
