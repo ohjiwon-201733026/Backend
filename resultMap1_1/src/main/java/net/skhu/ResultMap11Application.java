1faab8759b9f8a8551c3940b46f0653d5b8a3562
OH JIWON <ojw970725@gmail.com>
2020-11-29 15:54:51 +0900

    resultMap 연습

---
+++ resultMap1_1/src/main/java/net/skhu/ResultMap11Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultMap11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultMap11Application.class, args);
+	}
+
+}
