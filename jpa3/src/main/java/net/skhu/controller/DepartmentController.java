955271fd122f3e88c48217a267f04c9f60e4e70c
OH JIWON <ojw970725@gmail.com>
2020-12-10 17:16:36 +0900

    collection-valued association

---
+++ jpa3/src/main/java/net/skhu/controller/DepartmentController.java
@@ -0,0 +1,26 @@
+package net.skhu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.skhu.repository.DepartmentRepository;
+
+@Controller
+public class DepartmentController {
+
+	@Autowired DepartmentRepository departmentRepository;
+
+	@RequestMapping("department/list1")
+	public String list1(Model model) {
+		model.addAttribute("departments",departmentRepository.findAll());
+		return "department/list1";
+	}
+
+	@RequestMapping("department/list2")
+	public String list2(Model model) {
+		model.addAttribute("departments",departmentRepository.findAll());
+		return "department/list2";
+	}
+}
---
+++ jpa3/src/main/java/net/skhu/entity/Department.java
@@ -0,0 +1,27 @@
+package net.skhu.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Department {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+
+	String name;
+	String shortName;
+	String phone;
+
+	@OneToMany(mappedBy="department")
+	List<Student> students;
+}
