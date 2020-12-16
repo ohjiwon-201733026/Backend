6d7963636db107ee5ad36969c34f43113a6a341a
OH JIWON <ojw970725@gmail.com>
2020-12-16 13:53:07 +0900

    기말1

---
+++ H201733026/src/main/java/net/skhu/entity/City.java
@@ -0,0 +1,29 @@
+package net.skhu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class City {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+
+	String name;
+	int population;
+	int area;
+
+	@ManyToOne
+	@JoinColumn(name = "districtId")
+	District district;
+
+
+}
